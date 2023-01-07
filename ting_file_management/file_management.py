0d055d612ae17fbc495094d1d0d42fbb29a40f9a
Joao Coelho <joaopaulomelocoelho@gmail.com>
2023-01-07 18:08:40 -0300

    implements function to read file txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import os.path
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.isfile(path_file):
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+
+    f = open(path_file, "r")
+    lines = f.read().splitlines()
+    f.close()
+    return lines
