94ab47b0307a1e823467a81deb636890290c9b40
Joao Coelho <joaopaulomelocoelho@gmail.com>
2023-01-07 17:24:58 -0300

    implement queue to read files

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,27 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
+
+    def is_empty(self):
+        return self._data == []
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.is_empty():
+            return self._data.pop(0)
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if not (index >= 0 and index < self.size()):
+            raise IndexError
+        else:
+            return self._data[index]
+
+    def size(self):
+        return len(self._data)
