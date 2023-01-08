b2c4a40ac7b06a679690363dbdb349e641fffb0d
Joao Coelho <joaopaulomelocoelho@gmail.com>
2023-01-08 12:40:53 -0300

    implements function that checks if words exists

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,33 @@
+def find_ocurrencies(file_lines, word):
+    result = []
+    for index, line in enumerate(file_lines):
+        if word.lower() in line.lower():
+            result.append({"linha": index + 1})
+
+    return result
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue_size = instance.size()
+    queue_range = range(queue_size)
+    result = []
+
+    for index in queue_range:
+        file_processed = instance.search(index)
+        search_result = {
+            "palavra": word,
+            "arquivo": file_processed["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        file_lines = file_processed["linhas_do_arquivo"]
+        ocurrencies = find_ocurrencies(file_lines, word)
+        search_result["ocorrencias"].extend(ocurrencies)
+
+        if search_result["ocorrencias"]:
+            result.append(search_result)
+
+    return result
 
 
 def search_by_word(word, instance):
