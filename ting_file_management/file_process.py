4d28bcad06ddb8ea74ce00585920ef9c111e1fcd
Joao Coelho <joaopaulomelocoelho@gmail.com>
2023-01-08 11:33:47 -0300

    implements function that transforms file content and process in queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    queue_size = instance.size()
+    queue_range = range(queue_size)
+
+    for index in queue_range:
+        file_processed = instance.search(index)
+        if file_processed["nome_do_arquivo"] == path_file:
+            return None
+
+    file_lines = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines
+    }
+
+    instance.enqueue(result)
+    return print(result)
 
 
 def remove(instance):
